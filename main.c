b1cd6b02c1dc4183bfc10287c85422265949e117
JQ <juequ@live.com>
2015-05-03 14:52:46 -0400

    Create project add main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+// 0. This is the main function.
+// 1. 1st aim is to test the other functions in the subfolders.
+
+#include "./lib_string/lib_string.h"
+
+int main()
+{
+// to test string functions in the ./lib_string subfolder
+str_test();
+
+return 0;
+}
